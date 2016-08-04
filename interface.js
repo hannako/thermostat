e998c33467d1475d222ca062fefcd547b98928b7
hannako <jessjones1@gmail.com>
2016-08-04 15:33:43 +0100

    challenge 8: wip weather api

--- interface.js
+++ interface.js
@@ -1,23 +1,38 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
+
   $('#temp_up').click(function() {
     thermostat.up();
-    $('#temperature').text(thermostat.getTemperature());
+    updateTemperature();
   });
+
   $('#temp_down').click(function() {
     thermostat.down();
-    $('#temperature').text(thermostat.getTemperature());
+    updateTemperature();
   });
+
   $('#temp_reset').click(function() {
     thermostat.reset();
-    $('#temperature').text(thermostat.getTemperature());
+    updateTemperature();
   });
+
   $('#powersave_on').click(function() {
     thermostat.switchPowerSaveOn();
     $("#power_save_status").text(" on");
   });
+
   $('#powersave_off').click(function() {
     thermostat.switchPowerSaveOff();
     $("#power_save_status").text(" off");
   });
+
+  function updateTemperature(){
+    $('#temperature').text(thermostat.getTemperature());
+    $('#temperature').attr('class', thermostat.energyUseReporter());
+  };
+
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=6b9d318fe42ee8c5f85ba016a32d5492&units=metric', function(data){
+    $('#current_temperature').text(data.main.temp);
+  })
+
 });
