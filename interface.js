143524530c7784de3c46ee6076d458ae08d8716b
Frunez <francesco.nunez@gmail.com>
2016-08-04 12:48:37 +0100

    ch7 created functional buttons with jquery

--- interface.js
+++ interface.js
@@ -1,6 +1,23 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
-  $('#temperature').text(thermostat.temperature);
-
-
-})
+  $('#temp_up').click(function() {
+    thermostat.up();
+    $('#temperature').text(thermostat.getTemperature());
+  });
+  $('#temp_down').click(function() {
+    thermostat.down();
+    $('#temperature').text(thermostat.getTemperature());
+  });
+  $('#temp_reset').click(function() {
+    thermostat.reset();
+    $('#temperature').text(thermostat.getTemperature());
+  });
+  $('#powersave_on').click(function() {
+    thermostat.switchPowerSaveOn();
+    $("#power_save_status").text(" on");
+  });
+  $('#powersave_off').click(function() {
+    thermostat.switchPowerSaveOff();
+    $("#power_save_status").text(" off");
+  });
+});
---
+++ test.js
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Demo</title>
+</head>
+<body>
+    <a href="http://jquery.com/">jQuery</a>
+    <script src="jquery.js"></script>
+    <script>
+
+    // Your code goes here.
+      $("up").click(function(){
+        thermostat.up();
+      });
+    </script>
+</body>
+</html>
